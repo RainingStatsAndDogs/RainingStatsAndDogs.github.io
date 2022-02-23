b4346f31df4dddc7c500a31d067dfd8093ae4e8d
RainingStatsAndDogs <100037062+RainingStatsAndDogs@users.noreply.github.com>
2022-02-23 07:05:37 -0800

    changing folder structure, minor changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,14 +18,27 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
+         ('R', 'https://www.cran.org/'),
+         ('Julia', 'https://julialang.org/'),
          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+         ('scikit-learn', 'https://scikit-learn.org/stable/'),
+         ('statsmodels', 'https://www.statsmodels.org/stable/index.html),
+         ('pymc', 'https://docs.pymc.io/en/v3/'),
+         ('numpyro', 'https://num.pyro.ai/en/latest/index.html#'),
+         ('Stan', 'https://mc-stan.org/'),
+         ('tidyverse', 'https://www.tidyverse.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/RainingStatsAndDogs'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+MARKUP = ("md", "ipynb")
+
+from pelican_jupyter import markup as nb_markup
+PLUGINS = [nb_markup]
+
+IGNORE_FILES = [".ipynb_checkpoints"]
